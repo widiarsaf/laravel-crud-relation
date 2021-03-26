2f4a1c3d2ddf095602ece85684d831190b00a362
widiarsaf <wretasafitri33@gmail.com>
2021-03-26 12:02:17 +0700

    fixing bug/error in routes and controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
+use DB;
 
 class StudentController extends Controller
 {
@@ -31,18 +33,19 @@ class StudentController extends Controller
         ]);
 
         // eloquent function to add data
-        Student::create($request-all());
+        Student::create($request->all());
 
         // if the data is added successfully, will return to the main page
         return redirect()->route('student.index')
             ->with('success', 'Student Successfully Added');
+
     }
 
     public function show($Nim)
     {
         // displays detailed data by finding data by student nim
         $Student = Student::find($Nim);
-        return view('student.detail', compact('student'));
+        return view('student.detail', compact('Student'));
     }
 
     public function edit($Nim)
@@ -64,7 +67,7 @@ class StudentController extends Controller
         ]);
 
         // Eloquent function to update the data
-        Student::find($Nim)->update('request'->all());
+        Student::find($Nim)->update($request->all());
 
         // id the data successfully update, will return to main page
         return redirect()->route('student.index')
@@ -74,6 +77,7 @@ class StudentController extends Controller
     public function destroy($Nim)
     {
         // Eloquent function to delete the data
+        Student::find($Nim)->delete();
         return redirect()->route('student.index')
             ->with('success', 'Student Succesfully Deleted');
     }
