22638f0ceaedaa1aea41139877c8ad6757080f0f
widiarsaf <wretasafitri33@gmail.com>
2021-03-26 11:25:17 +0700

    Create student migration

---
+++ database/migrations/2021_03_26_042055_create_student_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id('id_student');
+            $table->string('nim', 10)->index();
+            $table->string('name', 25)->index();
+            $table->string('class', 5);
+            $table->string('major', 35);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+}
